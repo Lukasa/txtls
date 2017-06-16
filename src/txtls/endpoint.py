e89a1b23ca8e83136e57b8fd98d679a7c978fd99
Cory Benfield <lukasaoz@gmail.com>
2017-06-16 09:44:14 +0100

    Add a server endpoint

--- src/txtls/endpoint.py
+++ src/txtls/endpoint.py
@@ -3,7 +3,9 @@ from __future__ import absolute_import
 
 from zope.interface import implementer
 
-from twisted.internet.interfaces import IStreamClientEndpoint
+from twisted.internet.interfaces import (
+    IStreamClientEndpoint, IStreamServerEndpoint
+)
 
 from .factory import NativeMemoryTLSFactory
 
@@ -38,4 +40,29 @@ class PEP543ClientEndpoint(object):
         ).addCallback(lambda protocol: protocol.wrappedProtocol)
 
 
-# TODO: Server endpoint!
+@implementer(IStreamServerEndpoint)
+class PEP543ServerEndpoint(object):
+    """
+    An endpoint that takes an existing server stream endpoint and wraps it in
+    native TLS using PEP 543.
+    """
+    _wrapperFactory = NativeMemoryTLSFactory
+
+    def __init__(self, wrappedEndpoint, configuration, backend):
+        self._wrappedEndpoint = wrappedEndpoint
+        self._configuration = configuration
+        self._backend = backend
+
+
+    def listen(self, protocolFactory):
+        """
+        Connect the given protocol factory and unwrap its result.
+        """
+        return self._wrappedEndpoint.listen(
+            self._wrapperFactory(
+                configuration=self._configuration,
+                isClient=False,
+                wrappedFactory=protocolFactory,
+                backend=self._backend,
+            )
+        ).addCallback(lambda protocol: protocol.wrappedProtocol)
